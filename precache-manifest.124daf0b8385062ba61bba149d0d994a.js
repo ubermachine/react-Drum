c4f342c40cee14a08e503155b529abe5b7dc86ce
ubermachine <shivamkatoch@outlook.com>
2020-01-30 01:02:03 +0530

    Updates

---
+++ precache-manifest.124daf0b8385062ba61bba149d0d994a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44e4fc1f0463f34e9e6ef45835a56df",
+    "url": "/react-Drum/index.html"
+  },
+  {
+    "revision": "0b72998ecbb924cbf0f3",
+    "url": "/react-Drum/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0c27daec6a4ef8f0bb16",
+    "url": "/react-Drum/static/css/main.4f9b412e.chunk.css"
+  },
+  {
+    "revision": "0b72998ecbb924cbf0f3",
+    "url": "/react-Drum/static/js/2.4623c7fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-Drum/static/js/2.4623c7fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c27daec6a4ef8f0bb16",
+    "url": "/react-Drum/static/js/main.db81eb7f.chunk.js"
+  },
+  {
+    "revision": "9089a45eff9041bd8d06",
+    "url": "/react-Drum/static/js/runtime-main.57b0c446.js"
+  }
+]);
